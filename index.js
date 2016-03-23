e2006b4a0db1c4b2b07214666efe79168d5422e8
Paul Kohlhoff <paul.kohlhoff@gmail.com>
2016-03-23 08:51:01 -0600

    init my custom version of concat-cli

---
+++ index.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+var exampleTxt = 'This will concatenate all the js files in the current directory into a bundle.js';
+var argv = require('yargs')
+    .option('f', {
+        alias: 'files',
+        demand: true,
+        describe: 'files or glob/wildcard to be matched and concatenated',
+        type: 'string'
+    })
+    .option('o', {
+        alias: 'output',
+        default: 'all',
+        describe: 'the resulting file of the concatenation',
+        type: 'string'
+    })
+    .usage('$0 concat-cli -f string -o string')
+    .example('concat-cli -f *.js -o bundle.js', exampleTxt)
+    .help('help')
+    .argv;
+
+var concatFunctions = require('./concatFunctions');
+var glob = require("glob")
+
+glob(argv.f, function (er, files) {
+    concatFunctions.concatFiles(files, argv.o);
+})
+
+
